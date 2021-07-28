f870d38d602b92b59a63a048cc0b39a1f039cf0f
Yasin Doyle <lhh145@163.com>
2021-07-29 00:41:21 +0800

    二叉搜索树的后序遍历序列，添加自己的解法

--- 面试题33：二叉搜索树的后序遍历序列/二叉搜索树的后序遍历序列.cpp
+++ 面试题33：二叉搜索树的后序遍历序列/二叉搜索树的后序遍历序列.cpp
@@ -19,6 +19,52 @@ bool VerifySquenceOfBST(int sequence[], int length){
     bool left = true;
     if(i>0) left = VerifySquenceOfBST(sequence,i);
     bool right = true;
-    if(i<length -1) VerifySquenceOfBST(sequence+i,length-1-i);
+    if(i<length -1) VerifySquenceOfBST(sequence+i,length-1-i);//length-2-(i)+1
     return left && right;
 }
+//自己的版本
+bool myVerCore(int sequence[], int beginidx, int endidx)
+{
+    if(beginidx == endidx) return true;
+    int root = sequence[endidx];
+    int leftEnd = beginidx;
+    for(; leftEnd < endidx; leftEnd++){
+        if(sequence[leftEnd] > root) break;
+    }
+    int rightBegin = leftEnd;
+    for(; rightBegin < endidx; rightBegin++){
+        if(sequence[rightBegin] < root) return false;
+    }
+    return myVerCore(sequence,beginidx,leftEnd-1) && myVerCore(sequence,leftEnd,endidx-1);
+}
+
+bool myVer(int sequence[], int length)
+{
+    if(sequence == nullptr || length <= 0) return false;
+    return myVerCore(sequence,0,length-1);
+}
+
+//LeetCode版本
+#include<vector>
+using namespace std;
+class Solution{
+public:
+    bool core(vector<int> &postOrder, int beginidx, int endidx){
+        if(beginidx >= endidx ) return true;//这里考虑了两种情况beginidx == endidx 正常收缩， beginidx > endidx 只有右子树的情况 
+        int root = postOrder[endidx];
+        int leftEnd = beginidx;
+        for(;leftEnd<endidx;leftEnd++){
+            if(postOrder[leftEnd] > root) break;
+        }
+        int rightBegin = leftEnd;
+        for(;rightBegin<endidx;rightBegin++){
+            if(postOrder[rightBegin] < root) return false;
+        }
+        return core(postOrder,beginidx,leftEnd-1) && core(postOrder,leftEnd,endidx-1);
+    }
+    bool verifyPostorder(vector<int> &postorder){
+        if(postorder.empty()) return false;
+        int len = postorder.size();
+        return core(postorder,0,len-1);
+    }
+};
