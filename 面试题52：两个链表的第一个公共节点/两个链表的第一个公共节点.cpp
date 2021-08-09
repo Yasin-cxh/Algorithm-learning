30fadc3b3c1cf864ef89039b69d9f9d607a6d6eb
Yasin Doyle <lhh145@163.com>
2021-08-09 20:53:18 +0800

    两个链表的第一个公共节点

---
+++ 面试题52：两个链表的第一个公共节点/两个链表的第一个公共节点.cpp
@@ -0,0 +1,51 @@
+//题目
+//输入两个链表，找出它们的第一个公共节点
+//1.遍历两遍
+//第一遍获取两个链表的长度
+//长的链表先走len1-len2距离
+//如果遇到非空节点相同，就是第一个公共节点
+#include<vector>
+#include<string>
+using namespace std;
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {}
+ };
+ 
+class Solution {
+public:
+    int getListLength(ListNode *head){
+        int len = 0;
+        ListNode *pNode = head;
+        while(pNode != nullptr){
+            pNode = pNode->next;
+            len++;
+        }
+        return len;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA= getListLength(headA);
+        int lenB= getListLength(headB);
+        int distance;
+        ListNode *pShortHead, *pLongHead;
+        if(lenA < lenB){
+            pShortHead = headA;
+            pLongHead = headB;
+            distance  = lenB-lenA;
+        }else{
+            pShortHead = headB;
+            pLongHead = headA;
+            distance = lenA-lenB;
+        }
+        for(int i =0; i <distance; i++ ){
+            pLongHead = pLongHead->next;
+        }
+        while(pLongHead != nullptr && pShortHead != nullptr && pLongHead != pShortHead){
+            pLongHead = pLongHead->next;
+            pShortHead = pShortHead->next;
+        }
+        return pLongHead;
+
+    }
+};
