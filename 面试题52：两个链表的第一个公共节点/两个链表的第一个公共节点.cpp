d18af1285b5b29cad1ba3852ac5e60ed2010f2e2
Yasin Doyle <lhh145@163.com>
2021-08-30 12:21:59 +0800

    添加简单写法

--- 面试题52：两个链表的第一个公共节点/两个链表的第一个公共节点.cpp
+++ 面试题52：两个链表的第一个公共节点/两个链表的第一个公共节点.cpp
@@ -49,3 +49,16 @@ public:
 
     }
 };
+//简单写法
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+            if(headA == nullptr || headB == nullptr) return nullptr;
+            ListNode *n1 = headA, *n2 = headB;
+            while(n1 != n2){
+                n1 = n1==nullptr?headB:n1->next;
+                n2 = n2==nullptr?headA:n2->next;
+            }
+            return n1;
+    }
+};
